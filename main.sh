6b9b4b736a44ae7f26d459115ef4d39c96d5e2c7
Rafael Melo Reis <rafaelmeloreisnovo@gmail.com>
2025-03-29 01:07:34 -0300

    Adiciona main.sh ao repositório

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "----------------------------------"
+echo " Executando main.sh com sucesso! "
+echo " Ambiente Termux totalmente ativo "
+echo "----------------------------------"
+
+# Exemplo de operação automatizada
+date
+whoami
+node -v
